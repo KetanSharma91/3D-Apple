1da38243a003c66db666dac22aacf1e109e367ab
Ketan Sharma <ketansharma.ks.911@gmail.com>
2026-01-14 14:42:19 +0530

    commit4

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/3D-Apple/',
-  plugins: [react(), sentryVitePlugin({
-    org: "jsm-om",
-    project: "javascript-react"
-  })],
+  plugins: [react()],
+  // plugins: [react(), sentryVitePlugin({
+  //   org: "jsm-om",
+  //   project: "javascript-react"
+  // })],
 
-  build: {
-    sourcemap: true
-  }
+  // build: {
+  //   sourcemap: true
+  // }
 })
